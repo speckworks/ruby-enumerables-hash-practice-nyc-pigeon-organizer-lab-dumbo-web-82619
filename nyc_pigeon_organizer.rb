ee06e246c8ad5be2ffce2254fd941a3afe024694
John Karrell Speck <speckworks@gmail.com>
2019-08-19 21:55:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  new_pigeon_hash = {}
+    pigeon_data.each do |pigeon, pigeon_attribute|
+      new_pigeon_hash[pigeon] ||= pigeon_attribute
+      new_pigeon_hash[pigeon_attribute]
+    end
 end
+    
+
